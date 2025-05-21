b13d80503aa8270265f333bbb7084ebe8036eaac
kingkingkingkingkingkingking <kingstonrydzik@students.berkeley.net>
2025-05-20 18:33:50 -0700

    added 2 items

--- src/main/java/net/kingtaun/tutorialmod/TutorialMod.java
+++ src/main/java/net/kingtaun/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.kingtaun.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kingtaun.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 // testing repo
@@ -12,6 +13,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
